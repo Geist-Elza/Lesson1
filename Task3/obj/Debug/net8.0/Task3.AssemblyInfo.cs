069881199ab67d1fba0c25d7bdf1efceee8cf120
Geist-Elza <geist.elza@yandex.ru>
2024-01-07 10:54:35 +0700

    add Lesson1:task3

--- Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b04122bc624333375fe33824dda90861b0ff0dd")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
