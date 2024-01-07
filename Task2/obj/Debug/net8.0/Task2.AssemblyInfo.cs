8b04122bc624333375fe33824dda90861b0ff0dd
Geist-Elza <geist.elza@yandex.ru>
2024-01-07 08:39:13 +0700

    add lesson1:task2

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cf7d7027dfd0d8f3eb38f70939dd47f61e78174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ad5adf97bdb1aa0c6ed9bee9aac1fa3ffe0dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
